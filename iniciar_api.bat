034a5232af1fc91e4918b6f522ac930320884438
claudito <megabyt1507@gmail.com>
2026-09-15 10:13:33 -0500

    Update API startup script to change port from 8000 to 8888

--- iniciar_api.bat
+++ iniciar_api.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d D:\Proyectos\Python\ZteSmsGateway
-python -m uvicorn api:app --host 0.0.0.0 --port 8000 >> log.txt 2>&1
+python -m uvicorn api:app --host 0.0.0.0 --port 8888 >> log.txt 2>&1
